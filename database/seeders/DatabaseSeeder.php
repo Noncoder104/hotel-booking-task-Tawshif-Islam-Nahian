13fb5ccd3079b5ca601dc86d6ff5d92d9dfcf3cc
MD Tawshif Islam Nahian <ntawshif@gmail.com>
2025-10-27 03:22:37 +0600

    Added database schema (migrations/seeder) and initial controller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+       DB::table('room_categories')->insert([
+        ['name' => 'Premium Deluxe', 'base_price' => 12000], // [cite: 8]
+        ['name' => 'Super Deluxe', 'base_price' => 10000],  // [cite: 9]
+        ['name' => 'Standard Deluxe', 'base_price' => 8000], // [cite: 10]
+       
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+       
+       // ]);
+       ]);
     }
 }
